4f07f50ef7430e91b69dff78795ee835a1a71455
Relfos <relfos@gmail.com>
2018-05-08 13:27:53 +0100

    Initial commit

---
+++ CSSAtlasGen/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Lunar.Utils;
+
+namespace CSSAtlasGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var folder = @"D:\code\Sites\Phantasma_Site\team";
+            var filter = "*.jpg";
+            var resize = 240;
+            var outputPrefix = "team";
+            var outPicName = outputPrefix + ".jpg";
+            var outCSSName = outputPrefix + ".css";
+
+            var files = Directory.GetFiles(folder, filter);
+
+            var images = new Dictionary<string, Bitmap>();
+
+            var packer = new RectanglePacker<string>();
+            int count = 0;
+
+            int avgWidth = 0;
+            int avgHeight = 0;
+
+            foreach (var file in files)
+            {
+                var img = new Bitmap(Bitmap.FromFile(file));
+
+                if (resize != 0)
+                {
+                    img = new Bitmap(img, new Size(resize, resize));
+                }
+
+                images[file] = img;
+                packer.AddRect(img.Width, img.Height, file);
+                count++;
+
+                avgWidth += img.Width;
+                avgHeight += img.Height;
+            }
+            avgWidth /= count;
+            avgHeight /= count;
+
+            int side = (int)Math.Ceiling(Math.Sqrt(count));
+
+            int atlasWidth = avgWidth * side;
+            int atlasHeight = avgHeight * side;
+
+            int tot = packer.Pack(0, 0, atlasWidth, atlasHeight);
+
+            if (tot == 0)
+            {
+                var output = new Bitmap(atlasWidth, atlasHeight);
+
+                using (Graphics g = Graphics.FromImage(output))
+                {
+                    foreach (var file in files)
+                    {
+                        int x, y;
+                        packer.GetRect(file, out x, out y);
+
+                        var img = images[file];
+                        g.DrawImage(img, x, y, img.Width, img.Height);
+                    }                    
+                }
+
+                output.Save(outPicName);
+
+                Console.WriteLine("Generated " + outPicName);
+
+                var sb = new StringBuilder();
+
+                int index = 0;
+                foreach (var file in files)
+                {
+                    var name = Path.GetFileNameWithoutExtension(file).ToLower();
+
+                    if (index > 0)
+                    {
+                        sb.Append(", ");
+                    }
+
+                    if (index % side == side - 1)
+                    {
+                        sb.AppendLine();
+                    }
+
+                    sb.Append($".{outputPrefix}-{name}");
+
+                    index++;
+                }
+
+                sb.Append('{');
+                sb.AppendLine();
+                sb.AppendLine($"\tbackground-image: url('{outPicName}');");
+                sb.AppendLine("\tbackground-repeat: no-repeat;");
+                sb.Append('}');
+                sb.AppendLine();
+
+                foreach (var file in files)
+                {
+                    int x, y;
+                    packer.GetRect(file, out x, out y);
+
+                    var name = Path.GetFileNameWithoutExtension(file).ToLower();
+                    var img = images[file];
+
+                    sb.AppendLine();
+                    sb.AppendLine("."+outputPrefix+"-"+name+" {");
+                    sb.AppendLine($"\twidth: {img.Width}px;");
+                    sb.AppendLine($"\theight: {img.Height}px;");
+                    sb.AppendLine($"\tbackground-position: -{x}px -{y}px;");
+                    sb.Append('}');
+                    sb.AppendLine();
+                }
+
+                File.WriteAllText(outCSSName, sb.ToString());
+
+                Console.WriteLine("Generated " + outCSSName);
+            }
+            else
+            {
+                Console.WriteLine("Failed packing...");
+            }
+        }
+    }
+}
---
+++ CSSAtlasGen/RectanglePacker.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lunar.Utils
+{
+    public class RectanglePacker<T>
+    {
+        protected class PackerRect
+        {
+            public T key;
+            public int width;
+            public int height;
+            public bool done;
+            public PackerNode node;
+        }
+
+        protected class PackerNode
+        {
+            public PackerNode[] child; // 0..1
+            public int y;
+            public int x;
+            public int width;
+            public int height;
+            public PackerRect rect;
+
+            public PackerNode(int x, int y, int width, int height)
+            {
+                this.x = x;
+                this.y = y;
+                this.width = width;
+                this.height = height;
+                this.rect = null;
+                this.child = new PackerNode[2];
+                this.child[0] = null;
+                this.child[1] = null;
+            }
+
+            public PackerNode Insert(PackerRect rect)
+            {
+                PackerNode result;
+
+                if (child[0] != null && child[1] != null)
+                {
+                    result = child[0].Insert(rect);
+                    if (result != null)
+                        return result;
+
+                    result = child[1].Insert(rect);
+                    return result;
+                }
+
+
+                //if there's already a lightmap here, return
+                if (this.rect != null)
+                {
+                    return null;
+                }
+
+                // if we're too small, return
+                if (rect.width > this.width || rect.height > this.height)
+                {
+                    return null;
+                }
+
+                // if we're just right, accept
+                if (rect.width == this.width && rect.height == this.height)
+                {
+                    rect.done = true;
+                    this.rect = rect;
+                    return this;
+                }
+
+                // otherwise, gotta split this node and create some kids
+
+                //decide which way to split
+                int dw = this.width - rect.width;
+                int dh = this.height - rect.height;
+
+                if (dw > dh)
+                {
+                    this.child[0] = new PackerNode(this.x, this.y, rect.width, this.height);
+                    this.child[1] = new PackerNode(this.x + rect.width, this.y, this.width - (rect.width), this.height);
+                }
+                else
+                {
+                    this.child[0] = new PackerNode(this.x, this.y, this.width, rect.height);
+                    this.child[1] = new PackerNode(this.x, this.y + rect.height, this.width, this.height - (rect.height));
+                }
+
+                //insert into secondChannel child we created
+                return child[0].Insert(rect);
+            }
+        }
+
+        protected PackerNode _root;
+		protected List<PackerRect> _rectList;
+      
+	    public RectanglePacker()
+        {
+            ClearRects();
+        }
+
+        public void AddRect(int width, int height, T key)
+        {
+	        if (width<=0 || height<=0) 
+	        {
+		        throw new Exception();
+            }
+	
+	        PackerRect rect = new PackerRect();
+            _rectList.Add(rect);
+
+	        rect.width = width;
+	        rect.height = height;
+	        rect.key = key;
+	        rect.node = null;
+	        rect.done = false;
+        }
+
+        public bool GetRect(T key, out int x, out int y)
+        {
+            int n = -1;
+            for (int i=0; i<_rectList.Count; i++)
+            {
+                if (_rectList[i].key.Equals(key)) 
+                {
+                    n = i;
+                    break;
+                }
+            }
+
+            if (n<0 || _rectList[n].node == null || !_rectList[n].done) 
+            {
+                x = 0;
+                y = 0;
+                return false;
+            }
+    
+	        x = _rectList[n].node.x;
+	        y = _rectList[n].node.y;
+            return true;
+        }
+      
+        public void DeleteRect(T key)
+        {
+            int n = -1;
+            for (int i=0; i<_rectList.Count; i++)
+            {
+                if (_rectList[i].key.Equals(key)) 
+                {
+                    n = i;
+                    break;
+                }
+            }
+
+            if (n>=0) 
+            {
+                _rectList.RemoveAt(n);
+            }
+
+        }
+
+        public void ClearRects()
+        {
+            _rectList = new List<PackerRect>();
+            _root = null;               
+        }
+
+        /// <summary>
+        ///  Tries packing all added rects, and returns number of rects that could not be packed.
+        /// </summary>
+        /// <param name="minX"></param>
+        /// <param name="minY"></param>
+        /// <param name="maxX"></param>
+        /// <param name="maxY"></param>
+        /// <returns></returns>
+        public int Pack(int minX, int minY, int maxX, int maxY)
+        {
+            int count = 0;
+
+            for (int i = 0; i < _rectList.Count; i++)
+            {
+                if (!_rectList[i].done)
+                {
+                    count++;
+                }
+            }
+
+            int index = 0;
+
+            _root = null;
+
+	        while (count>0) 
+	        {
+                int max = 0;
+		        for (int i=0; i<_rectList.Count; i++)
+		        {
+			        if (_rectList[i].done) 
+                    {
+                        continue;
+                    }
+				        
+
+			        int k = _rectList[i].width * _rectList[i].height;
+			        if (k > max) 
+			        {
+        				max = k;
+				        index = i;
+                    }
+                }
+		    
+
+		        if (_root==null) 
+                {
+                    _root = new PackerNode(minX, minY, maxX, maxY);
+                }
+      
+
+		        _rectList[index].node = _root.Insert(_rectList[index]);
+                if (_rectList[index].node == null) 
+                {
+                    return count;
+                }
+
+		        count--;
+            }
+
+	        return count;
+        }
+    	 
+	}	
+}  
