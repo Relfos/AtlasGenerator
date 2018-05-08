191a3de167babf20998d60fe0d1b6ae57ed1ff3e
Relfos <relfos@gmail.com>
2018-05-08 13:35:59 +0100

    Added support for custom input args via command line

--- CSSAtlasGen/Program.cs
+++ CSSAtlasGen/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Text;
 using Lunar.Utils;
 
@@ -10,13 +9,55 @@ namespace CSSAtlasGen
 {
     class Program
     {
+        // example args: --folder=D:\code\Sites\Phantasma_Site\team --filter=*.jpg --prefix=team -output=jpg --resize=240
         static void Main(string[] args)
         {
-            var folder = @"D:\code\Sites\Phantasma_Site\team";
-            var filter = "*.jpg";
-            var resize = 240;
-            var outputPrefix = "team";
-            var outPicName = outputPrefix + ".jpg";
+            string folder = null;
+            string filter = "*.*";
+            string outputExtension = "jpg";
+            string outputPrefix = null;
+            int resize = 0;
+
+            foreach (var entry in args)
+            {
+                var arg = entry;
+
+                if (!arg.StartsWith("--"))
+                {
+                    Console.WriteLine("Invalid argument: " + arg);
+                    return;
+                }
+
+                arg = arg.Substring(2);
+
+                var temp = arg.Split(new char[] { '=' }, 2);
+
+                var key = temp[0].ToLower();
+                var val = temp.Length == 2 ? temp[1] : null;
+
+                switch (key)
+                {
+                    case "folder": folder = val; break;
+                    case "filter": filter = val; break;
+                    case "prefix": outputPrefix = val; break;
+                    case "output": outputExtension = val; break;
+                    case "resize": resize = int.Parse(val); break;
+                }
+            }
+
+            if (folder == null)
+            {
+                Console.WriteLine("Please specify a folder. Eg: --folder=some_path");
+                return;
+            }
+
+            if (outputPrefix == null)
+            {
+                Console.WriteLine("Please specify a atlas prefix. Eg: --prefix=something");
+                return;
+            }
+
+            var outPicName = outputPrefix + "." + outputExtension;
             var outCSSName = outputPrefix + ".css";
 
             var files = Directory.GetFiles(folder, filter);
@@ -68,7 +109,7 @@ namespace CSSAtlasGen
 
                         var img = images[file];
                         g.DrawImage(img, x, y, img.Width, img.Height);
-                    }                    
+                    }
                 }
 
                 output.Save(outPicName);
@@ -113,7 +154,7 @@ namespace CSSAtlasGen
                     var img = images[file];
 
                     sb.AppendLine();
-                    sb.AppendLine("."+outputPrefix+"-"+name+" {");
+                    sb.AppendLine("." + outputPrefix + "-" + name + " {");
                     sb.AppendLine($"\twidth: {img.Width}px;");
                     sb.AppendLine($"\theight: {img.Height}px;");
                     sb.AppendLine($"\tbackground-position: -{x}px -{y}px;");
